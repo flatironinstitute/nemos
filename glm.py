ec75adcf97291dcd940578edeba237564dfd5bcd
ahwillia <alex.h.willia@gmail.com>
2023-02-08 11:39:54 -0500

    jaxify

--- glm.py
+++ glm.py
@@ -1,35 +1,28 @@
-import numpy as np
+import jax.numpy as jnp
 from sklearn.linear_model import PoissonRegressor
 from scipy.ndimage import convolve1d
 from basis import MSpline
-import torch
-import torch.nn.functional as F
+import jax
+import jaxopt
 
 
-@torch.no_grad()
-def broadcast_convolve(v, B):
-    """
-    v : vector (num_timebins)
-    B : matrix (num_basis_funcs x num_timebins)
-    """
-    vt = len(v)
-    nb, bt = B.shape
-    return F.conv1d(
-        torch.from_numpy(np.tile(v[None, :], (nb, 1))),
-        torch.from_numpy(B[:, None, :]),
-        bias=None, stride=1,
-        groups=nb, padding=0
-    ).numpy()
+# Broadcasted 1d convolution operations.
+# [[n x t],[w]] -> [n x (t - w + 1)]
+_corr1 = jax.vmap(jax.numpy.correlate, (0, None), 0)
+# [[n x t],[p x w]] -> [n x p x (t - w + 1)]
+_corr2 = jax.vmap(_corr1, (None, 0), 0)
 
 
 class GLM:
 
-    def __init__(self, spike_basis, covariate_basis=None):
+    def __init__(self, spike_basis, covariate_basis=None, solver_name="LBFGS", solver_kwargs=dict()):
         self.spike_basis = spike_basis
         self.covariate_basis = covariate_basis
+        self.solver_name = solver_name
+        self.solver_kwargs = solver_kwargs
 
         # (num_basis_funcs x window_size)
-        self._B = self.spike_basis.transform()
+        self._spike_basis_matrix = self.spike_basis.transform()
 
     def fit(self, spike_data, covariates=None):
         """
@@ -49,30 +42,39 @@ class GLM:
 
         # Number of neurons and timebins
         nn, nt = spike_data.shape
+        nbs, nws = self._spike_basis_matrix.shape
+        
+        # Convolve spikes with basis functions.
+        #   Input shapes: (basis_funcs, timebins), (neurons, windowsize)
+        #   Output shape: (neurons, basis_funcs, timebins - windowsize + 1)
+        X = _corr2(self._spike_basis_matrix, spike_data)
 
-        # (num_neurons x num_basis_funcs x num_timebins)
-        X = np.stack(
-            [broadcast_convolve(neuron, self._B.T) for neuron in spike_data],
-            axis=0
+        # Initialize parameters
+        init_params = (
+            jnp.zeros((nn, nbs, nn)),  # Ws, spike basis coeffs
+            jnp.zeros(nn)              # bs, bias terms
         )
-        oo = nt - X.shape[-1]
-        print(X.shape)
 
+        # Poisson negative log-likelihood with an exponential link function.
+        #    TODO: other link functions.
+        def loss(params, X, y):
+            Ws, bs = params
+            log_pred = jnp.einsum("nbt,nbj->nt", X, Ws) + bs[:, None]
+            # log_pred = vmap(lambda A, B: A @ B)(X, Ws) + bs[:, None]
+            return jnp.sum(jnp.exp(log_pred) - y * log_pred)
 
-        # (num_neurons x num_basis_funcs)
-        self.coupling_coef_ = np.empty((nn, self.spike_basis.num_basis_funcs, nn))
-        self.coupling_intercept_ = np.empty(nn)
+        # Run optimization
+        solver = getattr(jaxopt, self.solver_name)(
+            fun=loss, **self.solver_kwargs
+        )
+        params, state = solver.run(
+            init_params, X=X, y=spike_data[:, (nws - 1):]
+        )
 
-        # Define sklearn model.
-        prg = PoissonRegressor(max_iter=1000)
+        # Store parameters
+        self._spike_basis_coeff = params[0]
+        self._baseline_log_fr = params[1]
 
-        # Fit parameters one neuron at a time.
-        for i, y in enumerate(spike_data):
-            prg.fit(X.reshape(-1, nt - oo).T, y=spike_data[i, oo:])
-            self.coupling_coef_[i] = prg.coef_.reshape(
-                self.spike_basis.num_basis_funcs, nn
-            )
-            self.coupling_intercept_[i] = prg.intercept_
 
     def predict(self, spike_data, covariates=None):
         """
@@ -84,23 +86,18 @@ class GLM:
         covariates : array (num_covariates x num_timebins)
             Other input variables (e.g. stimulus features)
         """
-
-        # TODO - add assert statement that self.fit(...) was called
-
-        # (num_neurons x num_basis_funcs x num_timebins)
-        X = np.stack(
-            [broadcast_convolve(neuron, self._B.T) for neuron in spike_data],
-            axis=0
-        )
-        return np.einsum("jbt,nbj->nt", X, self.coupling_coef_) + self.coupling_intercept_[:, None]
+        Ws = self._spike_basis_coeff
+        bs = self._baseline_log_fr
+        X = _corr2(self._spike_basis_matrix, spike_data)
+        log_pred = jnp.einsum("nbt,nbj->nt", X, Ws) + bs[:, None]
+        return jnp.exp(log_pred) # TODO: different link functions
 
 
 # Short test
 if __name__ == "__main__":
 
     nn, nt = 10, 1000
-    rs = np.random.RandomState(123)
-    spike_data = rs.poisson(np.ones((nn, nt))).astype("float64")
+    spike_data = jnp.ones((nn, nt))
 
     model = GLM(
         spike_basis=MSpline(num_basis_funcs=6, window_size=100, order=3),
