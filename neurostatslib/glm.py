fix bug, off-by-one error

neurostatslib/glm.py
@@ -48,7 +48,7 @@ class GLM:
         X = convolve_1d_basis(
             self._spike_basis_matrix,
             spike_data
-        )
+        )[:, :, :-1]
 
         # Initialize parameters
         if init_params is None:
@@ -72,7 +72,7 @@ class GLM:
         params, state = solver.run(
             init_params,
             X=X,
-            y=spike_data[:, (nws - 1):]
+            y=spike_data[:, nws:]
         )
 
         # Store parameters
@@ -133,7 +133,7 @@ class GLM:
                 spikes
             ) # X.shape == (num_neurons x num_basis_funcs x 1)
             fr = self.inverse_link_function(
-                jnp.einsum("nb,nbj->n", jnp.squeeze(X), Ws) + bs
+                jnp.einsum("nbz,nbj->n", X, Ws) + bs
             )
             new_spikes = jax.random.poisson(key, fr)
             concat_spikes = jnp.column_stack(

prototests/test_glm_synthetic_single_neuron.py
@@ -9,7 +9,7 @@ import itertools
 jax.config.update("jax_platform_name", "cpu")
 jax.config.update("jax_enable_x64", True)
 
-nn, nt, ws = 2, 5000, 100
+nn, nt, ws = 1, 5000, 100
 simulation_key = jax.random.PRNGKey(123)
 
 spike_basis = RaisedCosineBasis(
@@ -18,21 +18,14 @@ spike_basis = RaisedCosineBasis(
 )
 B = spike_basis.transform()
 
-w0 = onp.array([-.1, -.1, -.2, -.2, -1])
-w1 = onp.array([0, .1, .5, .1, 0])
-
-W = onp.empty((2, 5, 2))
-for i, j in itertools.product(range(nn), range(nn)):
-    W[i, :, j] = w0 if (i == j) else w1
-
 simulated_model = GLM(
     spike_basis=spike_basis,
     covariate_basis=None
 )
-simulated_model.spike_basis_coeff_ = jnp.array(W)
+simulated_model.spike_basis_coeff_ = jnp.array([0, 0, -1, -1, -1])[None, :, None]
 simulated_model.baseline_log_fr_ = jnp.ones(nn) * .1
 
-init_spikes = jnp.zeros((2, spike_basis.window_size))
+init_spikes = jnp.zeros((nn, spike_basis.window_size))
 spike_data = simulated_model.simulate(simulation_key, nt, init_spikes)
 sim_pred = simulated_model.predict(spike_data)
 
@@ -42,9 +35,9 @@ fitted_model = GLM(
     # solver_name="ScipyMinimize",
     # solver_kwargs=dict(method="newton-cg", maxiter=1000, options=dict(verbose=True)),
     solver_name="GradientDescent",
-    solver_kwargs=dict(maxiter=10000, acceleration=False, verbose=True, stepsize=-1)
+    solver_kwargs=dict(maxiter=10000, acceleration=False, verbose=True, stepsize=0.0)
     # solver_name="LBFGS",
-    # solver_kwargs=dict(maxiter=100, verbose=True, stepsize=-1)
+    # solver_kwargs=dict(maxiter=100, verbose=True, stepsize=0.0)
 )
 # fitted_model.fit(spike_data, init_params=(
 #     jnp.copy(simulated_model.spike_basis_coeff_),
@@ -53,25 +46,21 @@ fitted_model = GLM(
 fitted_model.fit(spike_data)
 fit_pred = fitted_model.predict(spike_data)
 
-fig, axes = plt.subplots(2, 1)
-axes[0].plot(onp.arange(nt), spike_data[0])
-axes[0].plot(onp.arange(ws, nt + 1), sim_pred[0])
-axes[0].plot(onp.arange(ws, nt + 1), fit_pred[0])
-axes[1].plot(onp.arange(nt), spike_data[1])
-axes[1].plot(onp.arange(ws, nt + 1), sim_pred[1])
-axes[1].plot(onp.arange(ws, nt + 1), fit_pred[1])
+fig, ax = plt.subplots(1, 1)
+ax.plot(onp.arange(nt), spike_data[0])
+ax.plot(onp.arange(ws, nt + 1), sim_pred[0])
+ax.plot(onp.arange(ws, nt + 1), fit_pred[0])
 plt.show()
 
-fig, axes = plt.subplots(nn, nn, sharey=True)
-for i, j in itertools.product(range(nn), range(nn)):
-    axes[i, j].plot(
-        B.T @ simulated_model.spike_basis_coeff_[i, :, j],
-        label="true"
-    )
-    axes[i, j].plot(
-        B.T @ fitted_model.spike_basis_coeff_[i, :, j],
-        label="est"
-    )
-    axes[i, j].axhline(0, dashes=[2, 2], color='k')
-axes[-1, -1].legend()
+fig, ax = plt.subplots(1, 1, sharey=True)
+ax.plot(
+    B.T @ simulated_model.spike_basis_coeff_[0, :, 0],
+    label="true"
+)
+ax.plot(
+    B.T @ fitted_model.spike_basis_coeff_[0, :, 0],
+    label="est"
+)
+ax.axhline(0, dashes=[2, 2], color='k')
+ax.legend()
 plt.show()