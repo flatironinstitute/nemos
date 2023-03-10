bbee94d80e4bba20c31321951895f4ba9228e0ed
William F. Broderick <billbrod@gmail.com>
2023-03-10 17:51:52 -0500

    updates basis.py with annotations, etc

--- neurostatslib/basis.py
+++ neurostatslib/basis.py
@@ -1,121 +1,168 @@
+"""Bases classes.
 """
-Python code to generate M-splines.
-
-References
-----------
-Ramsay, J. O. (1988). Monotone regression splines in action.
-Statistical science, 3(4), 425-441.
-"""
+# required to get ArrayLike to render correctly, unnecessary as of python 3.10
+from __future__ import annotations
 import numpy as np
 import scipy.linalg
+from typing import Tuple, Optional
+from numpy.typing import ArrayLike, NDArray
 
 
 class Basis:
-    def __init__(self, num_basis_funcs, support):
-        """
-        n : int
-            Number of basis functions.
-        support:
-            Domain of the basis functions.
-        """
-        self.num_basis_funcs = num_basis_funcs
-        self.support = support
+    """Generic class for basis functions
 
-    @property
-    def ndim(self):
-        return len(self.shape)
+    Parameters
+    ----------
+    n_basis_funcs
+        Number of basis functions.
+    window_size
+        Size of basis functions.
+    support
+        Domain of the basis functions.
+
+    """
+    def __init__(self, n_basis_funcs: int,
+                 window_size: int,
+                 support: Tuple[int, int]):
+        self.n_basis_funcs = n_basis_funcs
+        self.window_size = window_size
+        self.support = support
 
     def check_in_support(self, x):
         raise NotImplementedError() # TODO
 
 
 class RaisedCosineBasis(Basis):
-    """
-    Raised cosine basis functions with log-spacing used by Pillow et al.
-    """
-    def __init__(self, *, num_basis_funcs, window_size):
-        self.num_basis_funcs = num_basis_funcs
-        self.window_size = window_size
+    """Raised cosine basis functions with log-spacing used by Pillow et al.
 
-    def transform(self, x=None):
+    Parameters
+    ----------
+    n_basis_funcs
+        Number of basis functions.
+    window_size
+        Size of basis functions.
+    """
+    def __init__(self, n_basis_funcs: int,
+                 window_size: int):
+        super().__init__(n_basis_funcs, window_size,
+                         (0, window_size))
+
+    def transform(self, x: Optional[ArrayLike] = None) -> NDArray:
+        """Generate basis functions with given spacing.
+
+        Parameters
+        ----------
+        x : (n_pts,)
+            Spacing for basis functions. If None, uses log-spacing.
+
+        Returns
+        -------
+        basis_funcs : (n_basis_funcs, n_pts)
+            Raised cosine basis functions
+
+        """
         if x is None:
             # linear spacing would be...
             # x = np.linspace(
-            #     0, np.pi * (self.num_basis_funcs - 1), self.window_size
+            #     0, np.pi * (self.n_basis_funcs - 1), self.window_size
             # )
             x = np.logspace(
-                np.log10(np.pi * (self.num_basis_funcs - 1)), -1, self.window_size
+                np.log10(np.pi * (self.n_basis_funcs - 1)), -1, self.window_size
             ) - .1
-
-        cx = x[None, :] - (np.pi * np.arange(self.num_basis_funcs))[:, None]
+        x = np.array(x, ndmin=1)
+        cx = x[None, :] - (np.pi * np.arange(self.n_basis_funcs))[:, None]
         return .5 * (np.cos(
             np.clip(cx, -np.pi, np.pi)
         ) + 1)
 
 
-class OrthExponentials(Basis):
-    """
-    Set of 1D basis functions that are decaying exponentials
-    numerically orthogonalized.
+class OrthExponentialBasis(Basis):
+    """Set of 1D basis functions that are decaying exponentials numerically
+    orthogonalized.
+
+    Parameters
+    ----------
+    decay_rates : (n_basis_funcs,)
+        Decay rates of the basis functions
+    window_size
+        Size of basis functions.
+
     """
 
-    def __init__(self, *, decay_rates, window_size):
-        super().__init__(len(decay_rates), (0, window_size))
+    def __init__(self, decay_rates: NDArray[np.floating],
+                 window_size: int):
+        super().__init__(len(decay_rates), window_size, (0, window_size))
         self.decay_rates = decay_rates
-        self.window_size = window_size
 
-    def transform(self, x=None):
-        """
+    def transform(self, x: Optional[ArrayLike] = None) -> NDArray:
+        """Generate basis functions with given spacing.
+
         Parameters
         ----------
-        x : ndarray
-            1d array, shape = (num_pts,), holding elements on the
-            interval [0, window_size).
+        x : (n_pts,)
+            Spacing for basis functions, holding elements on the interval [0,
+            window_size). If None, use a grid (``np.arange(self.window_size)``).
 
         Returns
         -------
-        vals : ndarray
-            2d array, shape = (num_basis_funcs, num_pts), holding
-            evaluated spline basis functions.
-        """
+        vals : (n_basis_funcs, n_pts)
+            Evaluated spline basis functions
 
-        # Default is to use a grid.
+        """
         if x is None:
             x = np.arange(self.window_size)
+        x = np.array(x, ndmin=1)
+        if x.min() < 0:
+            raise ValueError(f"values in x must lie within [0, {self.window_size})")
+        if x.max() >= self.window_size:
+            raise ValueError(f"values in x must lie within [0, {self.window_size})")
 
         # because of how scipy.linalg.orth works, have to create a matrix of
-        # shape (num_pts, num_basis_funcs) and then transpose, rather than
-        # directly computing orth on the matrix of shape (num_basis_funcs,
-        # num_pts)
+        # shape (n_pts, n_basis_funcs) and then transpose, rather than
+        # directly computing orth on the matrix of shape (n_basis_funcs,
+        # n_pts)
         return scipy.linalg.orth(
             np.stack([np.exp(-lam * x) for lam in self.decay_rates],
                      axis=1)
         ).T
 
 
-class MSpline(Basis):
-    """
-    M-spline 1-dimensional basis functions.
+class MSplineBasis(Basis):
+    """M-spline 1-dimensional basis functions.
+
+    Parameters
+    ----------
+    n_basis_funcs
+        Number of basis functions.
+    window_size
+        Size of basis functions.
+    order
+        Order of the splines used in basis functions. Must lie within [1,
+        n_basis_funcs].
+
 
     References
     ----------
-    Ramsay, J. O. (1988). Monotone regression splines in action.
-    Statistical science, 3(4), 425-441.
+    .. [1] Ramsay, J. O. (1988). Monotone regression splines in action.
+       Statistical science, 3(4), 425-441.
+
     """
 
-    def __init__(self, *, num_basis_funcs, window_size, order=2):
-        super().__init__(num_basis_funcs, (0, window_size))
+    def __init__(self, n_basis_funcs: int, window_size:int,
+                 order: int = 2):
+        super().__init__(n_basis_funcs, window_size, (0, window_size))
         self.order = order
-        self.window_size = window_size
 
         # Determine number of interior knots.
-        self.num_interior_knots = nik = num_basis_funcs - order
+        num_interior_knots = n_basis_funcs - order
 
+        if order < 1:
+            raise ValueError('Spline order must be non-negative!')
         # Check hyperparameters.
-        if nik < 0:
+        if num_interior_knots < 0:
             raise ValueError(
                 "Spline `order` parameter cannot be larger "
-                "than `num_basis_funcs` parameter."
+                "than `n_basis_funcs` parameter."
             )
 
         # Set of spline knots. We need to add extra knots to
@@ -129,44 +176,60 @@ class MSpline(Basis):
         # they want.... but this could be the default.
         self.knot_locs = np.concatenate((
             np.zeros(order - 1),
-            np.linspace(0, 1, nik + 2),
+            np.linspace(0, 1, num_interior_knots + 2),
             np.ones(order - 1),
         ))
 
-    def transform(self, x=None):
-        """
+    def transform(self, x: Optional[ArrayLike] = None) -> NDArray:
+        """Generate basis functions with given spacing.
+
         Parameters
         ----------
-        x : ndarray
-            1d array, shape = (num_pts,), holding elements on the
-            interval [0, window_size).
+        x : (n_pts,)
+            Spacing for basis functions, holding elements on the interval [0,
+            window_size). If None, use a grid (``np.arange(self.window_size)``).
 
         Returns
         -------
-        vals : ndarray
-            2d array, shape = (num_basis_funcs, num_pts), holding
-            evaluated spline basis functions.
-        """
+        vals : (n_basis_funcs, n_pts)
+            Evaluated spline basis functions
 
-        # Default is to use a grid.
+        """
         if x is None:
             x = np.arange(self.window_size)
+        x = np.array(x, ndmin=1)
+        if x.min() < 0:
+            raise ValueError(f"values in x must lie within [0, {self.window_size})")
+        if x.max() >= self.window_size:
+            raise ValueError(f"values in x must lie within [0, {self.window_size})")
 
         x = x / self.window_size
-        assert x.min() >= 0
-        assert x.max() < 1
 
         return np.stack(
-            [mspline(x, self.order, i, self.knot_locs) for i in range(self.num_basis_funcs)],
+            [mspline(x, self.order, i, self.knot_locs) for i in range(self.n_basis_funcs)],
             axis=0
         )
 
 
+def mspline(x: NDArray, k: int, i: int, T: NDArray):
+    """Compute M-spline basis function.
 
-def mspline(x, k, i, T):
-    """
-    Compute M-spline basis function `i` at points `x` for a spline
-    basis of order-`k` with knots `T`.
+    Parameters
+    ----------
+    x : (n_pts,)
+        Spacing for basis functions, holding elements on the interval [0,
+        window_size). If None, use a grid (``np.arange(self.window_size)``).
+    k
+        Order of the spline basis.
+    i
+        Number of the spline basis.
+    T : (k + n_basis_funcs,)
+        knot locations. should lie in interval [0, 1].
+
+    Returns
+    -------
+    spline : (n_pts,)
+        M-spline basis function.
     """
 
     # Boundary conditions.
@@ -200,7 +263,7 @@ if __name__ == "__main__":
     # for k, ax in enumerate(axes):
         
     #     # Create spline object.
-    #     spline = MSpline(num_basis_funcs=6, window_size=1000, order=(k + 1))
+    #     spline = MSplineBasis(n_basis_funcs=6, window_size=1000, order=(k + 1))
         
     #     # Transform and plot spline bases.
     #     ax.plot(spline.transform().T)
@@ -211,7 +274,7 @@ if __name__ == "__main__":
     # plt.show()
 
     # # Test for orthogonalized exponentials
-    # basis = OrthExponentials(
+    # basis = OrthExponentialBasis(
     #     decay_rates=np.logspace(-1, 0, 5),
     #     window_size=100
     # )
@@ -221,7 +284,7 @@ if __name__ == "__main__":
 
     # Test for raised cosines
     basis = RaisedCosineBasis(
-        num_basis_funcs=5,
+        n_basis_funcs=5,
         window_size=1000
     )
     fig, ax = plt.subplots(1, 1)
