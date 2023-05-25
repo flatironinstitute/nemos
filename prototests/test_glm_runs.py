0400dd0f7092fa71216cd3f6bcc98a7dd411b5f0
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2023-05-25 19:16:18 -0400

    created B-spline using scipy.interpolate.splev allowing derivatives evaluation;\n added cyclic B-spline;\n moved knots definition to separate method; allowed out of range samples ( 0s in the model matrix for those time points)

--- prototests/test_glm_runs.py
+++ prototests/test_glm_runs.py
@@ -1,7 +1,8 @@
 import jax
 from neurostatslib.glm import GLM
-from neurostatslib.basis import MSplineBasis
+from neurostatslib.basis import MSplineBasis, Cyclic_MSplineBasis
 import numpy as np
+import matplotlib.pylab as plt
 
 nn, nt = 10, 1000
 key = jax.random.PRNGKey(123)
@@ -10,11 +11,25 @@ spike_data = jax.random.bernoulli(
     subkey, jax.numpy.ones((nn, nt))*.5
 ).astype("int64")
 
-spike_basis = MSplineBasis(n_basis_funcs=6, window_size=100, order=3)
-spike_basis_matrix = spike_basis.gen_basis_funcs(np.arange(100))
-model = GLM(spike_basis_matrix)
+# spike_basis = MSplineBasis(n_basis_funcs=6, window_size=100, order=3)
+# spike_basis_matrix = spike_basis.gen_basis_funcs(np.arange(100)/100.)
+# spike_basis.generate_knots(np.arange(100)/100.,0.,1.)
+# spike_basis_matrix_splev = spike_basis.gen_basis_funcs_splev(np.arange(100)/100., outer_ok=False, der=0)
+#
+#
+# plt.close('all')
+# plt.figure()
+# plt.plot(spike_basis_matrix_splev.T)
 
-model.fit(spike_data)
-model.predict(spike_data)
-key, subkey = jax.random.split(key)
-X = model.simulate(subkey, 20, spike_data[:, :100])
+c_basis = Cyclic_MSplineBasis(n_basis_funcs=11, window_size=100, order=4)
+spike_basis_matrix_splev = c_basis.gen_basis_funcs_splev(np.arange(100)/100., der=0)
+
+print(c_basis.n_basis_funcs, spike_basis_matrix_splev.shape[0])
+plt.figure()
+plt.plot(spike_basis_matrix_splev.T)
+# model = GLM(spike_basis_matrix)
+#
+# model.fit(spike_data)
+# model.predict(spike_data)
+# key, subkey = jax.random.split(key)
+# X = model.simulate(subkey, 20, spike_data[:, :100])
