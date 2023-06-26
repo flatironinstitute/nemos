4724e0e473eb5b394b839c99123b304b73560e10
Edoardo Balzani <ebalzani@ccnlin052.flatironinstitute.org>
2023-06-26 12:08:32 -0400

    fixed plot close all

--- tests/test_glm_synthetic_single_neuron.py
+++ tests/test_glm_synthetic_single_neuron.py
@@ -49,7 +49,7 @@ def test_glm_fit():
     ax.plot(onp.arange(ws, nt + 1), sim_pred[0])
     ax.plot(onp.arange(ws, nt + 1), fit_pred[0])
     
-    #plt.close('all')
+    
     fig, ax = plt.subplots(1, 1, sharey=True)
     ax.plot(
         B.T @ simulated_model.spike_basis_coeff_[0, :, 0],
@@ -62,4 +62,4 @@ def test_glm_fit():
     ax.axhline(0, dashes=[2, 2], color='k')
     ax.legend()
     
-    #plt.close('all')
+    plt.close('all')
