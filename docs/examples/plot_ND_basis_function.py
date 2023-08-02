6f488bfb880ceb5d0b26ebc515f492a8013f6d04
Edoardo Balzani <edoardo.balzani87@gmail.com>
2023-08-02 14:53:27 -0400

    Update docs/examples/plot_ND_basis_function.py
    Co-authored-by: William F. Broderick <billbrod@gmail.com>

--- docs/examples/plot_ND_basis_function.py
+++ docs/examples/plot_ND_basis_function.py
@@ -24,7 +24,7 @@ These basis functions can be combined in the following ways:
 
 1. **Addition:** If we assume that there is no interaction between the stimuli, the response function can be adequately described by the sum of the individual components. The function is defined as:
    $$
-   f(\mathbf{x}, \mathbf{y}) \\approx \sum_{i} \\alpha_{i} \, a_i (\mathbf{x})  + \sum_{j} \\beta_j b_j(\mathbf{y}).
+   f(\mathbf{x}, \mathbf{y}) \\approx \sum_{i=0}^{k-1} \\alpha_{i} \, a_i (\mathbf{x})  + \sum_{j=0}^{h-1} \\beta_j b_j(\mathbf{y}).
    $$
    The resulting additive basis simply consists of the concatenation of the two basis sets: $$[A_0 (\mathbf{x}, \mathbf{y}), ..., A_{k+h-1} (\mathbf{x}, \mathbf{y})],$$ where
    $$
