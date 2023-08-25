b9b5b85b81f8332278cc1878172fb94c80375984
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2023-08-25 14:49:48 -0400

    added docstrings to model_base.py

--- tests/test_model_base.py
+++ tests/test_model_base.py
@@ -7,8 +7,12 @@ from neurostatslib.base_class import BaseRegressor  # Adjust this import to your
 
 # Sample subclass to test instantiation and methods
 class MockBaseRegressor(BaseRegressor):
-
+    """
+    Mock implementation of the BaseRegressor abstract class for testing purposes.
+    Implements all required abstract methods as empty methods.
+    """
     def __init__(self, std_param: int = 0, **kwargs):
+        """Initialize a MockBaseRegressor instance with optional standard parameters."""
         self.std_param = std_param
         super().__init__(**kwargs)
 
@@ -34,7 +38,8 @@ class MockBaseRegressor(BaseRegressor):
 
 class MockBaseRegressor_Invalid(BaseRegressor):
     """
-    Mock model that doesn't implement `_score` abstract method
+    Mock model that intentionally doesn't implement all the required abstract methods.
+    Used for testing the instantiation of incomplete concrete classes.
     """
 
     def __init__(self, std_param: int = 0, **kwargs):
@@ -49,53 +54,69 @@ class MockBaseRegressor_Invalid(BaseRegressor):
 
 
 def test_init():
+    """Test the initialization of the MockBaseRegressor class."""
     model = MockBaseRegressor(param1="test", param2=2)
     assert model.param1 == "test"
     assert model.param2 == 2
     assert model.std_param == 0
 
+
 def test_get_params():
+    """Test the get_params method."""
     model = MockBaseRegressor(param1="test", param2=2)
     params = model.get_params(deep=True)
     assert params["param1"] == "test"
     assert params["param2"] == 2
     assert params["std_param"] == 0
 
+
 def set_params():
+    """Test the set_params method."""
     model = MockBaseRegressor(param1="init_param")
     model.set_params(param1="changed")
     model.set_params(std_param=1)
     assert model.param1 == "changed"
     assert model.std_param == 1
 
+
 def test_invalid_set_params():
+    """Test invalid parameter setting using the set_params method."""
     model = MockBaseRegressor()
     with pytest.raises(ValueError):
         model.set_params(invalid_param="invalid")
 
+
 def test_get_param_names():
+    """Test retrieval of parameter names using the _get_param_names method."""
     param_names = MockBaseRegressor._get_param_names()
     # As per your implementation, _get_param_names should capture the constructor arguments
     assert "std_param" in param_names
 
+
 def test_convert_to_jnp_ndarray():
+    """Test data conversion to JAX NumPy arrays."""
     data = [1, 2, 3]
     jnp_data, = BaseRegressor._convert_to_jnp_ndarray(data)
     assert isinstance(jnp_data, jnp.ndarray)
     assert jnp.all(jnp_data == jnp.array(data, dtype=jnp.float32))
 
+
 def test_has_invalid_entry():
+    """Test validation of data arrays."""
     valid_data = jnp.array([1, 2, 3])
     invalid_data = jnp.array([1, 2, jnp.nan])
     assert not BaseRegressor._has_invalid_entry(valid_data)
     assert BaseRegressor._has_invalid_entry(invalid_data)
 
+
 # To ensure abstract methods aren't callable
 def test_abstract_class():
+    """Ensure that abstract methods aren't callable."""
     with pytest.raises(TypeError, match="Can't instantiate abstract"):
         BaseRegressor()
 
 def test_invalid_concrete_class():
+    """Ensure that classes missing implementation of required abstract methods raise errors."""
     with pytest.raises(TypeError, match="Can't instantiate abstract"):
         model = MockBaseRegressor_Invalid()
 
