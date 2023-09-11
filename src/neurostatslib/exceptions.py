f56bd4ff5b6184baaa499320c9ad5c69b957eee2
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2023-09-11 19:21:11 -0400

    fixed example

--- src/neurostatslib/exceptions.py
+++ src/neurostatslib/exceptions.py
@@ -9,10 +9,10 @@ class NotFittedError(ValueError, AttributeError):
 
     Examples
     --------
-    >>> from neurostatslib.glm import PoissonGLM
+    >>> from neurostatslib.glm import GLM
     >>> from neurostatslib.exceptions import NotFittedError
     >>> try:
-    ...     PoissonGLM().predict([[[1, 2], [2, 3], [3, 4]]])
+    ...     GLM().predict([[[1, 2], [2, 3], [3, 4]]])
     ... except NotFittedError as e:
     ...     print(repr(e))
 
