1fc86b285cebb04692050ee3b3c044cbbab37750
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2023-11-30 09:52:19 -0500

    test_fit_time_points_y

--- tests/test_glm.py
+++ tests/test_glm.py
@@ -279,21 +279,16 @@ class TestGLM:
         with expectation:
             model.fit(X, y, init_params=true_params)
 
-    @pytest.mark.parametrize("delta_tp, error, match_str",
-                             [
-                                 (-1, ValueError, "The number of time-points in X and y"),
-                                 (0, None, None),
-                                 (1, ValueError, "The number of time-points in X and y")
-                             ]
-                             )
-    def test_fit_time_points_y(self, delta_tp, error, match_str, poissonGLM_model_instantiation):
-        """
-        Test the `fit` method for inconsistencies in time-points in y. Ensure the correct number of time-points.
-        """
+    @pytest.mark.parametrize("delta_tp, expectation", [
+        (-1, pytest.raises(ValueError, match="The number of time-points in X and y")),
+        (0, does_not_raise()),
+        (1, pytest.raises(ValueError, match="The number of time-points in X and y"))
+    ])
+    def test_fit_time_points_y(self, delta_tp, expectation, poissonGLM_model_instantiation):
         X, y, model, true_params, firing_rate = poissonGLM_model_instantiation
-        n_samples, n_neurons, n_features = X.shape
         y = jnp.zeros((y.shape[0] + delta_tp,) + y.shape[1:])
-        _test_class_method(model, "fit", [X, y], {"init_params": true_params}, error, match_str)
+        with expectation:
+            model.fit(X, y, init_params=true_params)
 
     def test_fit_mask_grouplasso(self, group_sparse_poisson_glm_model_instantiation):
         """Test that the group lasso fit goes through"""
