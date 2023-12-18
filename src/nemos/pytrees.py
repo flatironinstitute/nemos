49c4c055a002213ca754bb32c8f640990e2683c9
William F. Broderick <billbrod@gmail.com>
2023-12-18 18:18:08 -0500

    tries having best of both worlds
    Puts the value/shape validation back in FeaturePytree (which is
    necessary for cross-validation), but adds an except statement in
    tree_unflatten which will allow us to use tree_map and return a standard
    dict if we can't return a FeaturePytree

--- src/nemos/pytrees.py
+++ src/nemos/pytrees.py
@@ -17,24 +17,37 @@ class FeaturePytree(UserDict):
 
     """
     def __init__(self, **kwargs):
+        self._num_time_points = None
         super().__init__(kwargs)
 
     @property
     def shape(self):
-        # the only shape we know is the number of features we have, as returned
-        # by len
-        return (self.__len__(), )
+        # Every value is an array with the same number of time points, so
+        # that's the only shared shape.
+        return (self._num_time_points, )
 
-    @property
-    def ndim(self):
-        # You can only slice into FeaturePytree along the time dimension, so it
-        # has dimensionality one.
-        return 1
+    def __len__(self):
+        # Same logic as shape
+        return self._num_time_points
 
     def __setitem__(self, key, value):
         # All keys are strings
         if not isinstance(key, str):
             raise ValueError("keys must be strings!")
+        # All values are array-like and must have time points along the first
+        # dimension, so double-check that's the case here.
+        try:
+            value.shape[0]
+        except (AttributeError, IndexError):
+            raise ValueError("All values must be arrays of at least 1 dimension!")
+        # For the first value, we must set how time points this FeaturePytree
+        # has
+        if self._num_time_points is None:
+            self._num_time_points = value.shape[0]
+        # For all subsequent values, ensure they have the proper number of time points
+        else:
+            if self._num_time_points != value.shape[0]:
+                raise ValueError(f"All arrays must have same number of time points, {self._num_time_points}")
         super().__setitem__(key, value)
 
     def __getitem__(self, key):
@@ -48,12 +61,7 @@ class FeaturePytree(UserDict):
 
     def __repr__(self):
         # Show the shape and data type of each array
-        repr = []
-        for k, v in self.data.items():
-            try:
-                repr.append(f'{k}: shape {v.shape}, dtype {v.dtype}')
-            except AttributeError:
-                repr.append(f'{k}: {v}')
+        repr = [f'{k}: shape {v.shape}, dtype {v.dtype}' for k, v in self.data.items()]
         return '\n'.join(repr)
 
     def tree_flatten(self):
@@ -61,4 +69,7 @@ class FeaturePytree(UserDict):
 
     @classmethod
     def tree_unflatten(cls, aux_data, children):
-        return cls(**jax.tree_util.tree_unflatten(aux_data, children))
+        try:
+            return cls(**jax.tree_util.tree_unflatten(aux_data, children))
+        except ValueError:
+            return dict(**jax.tree_util.tree_unflatten(aux_data, children))
