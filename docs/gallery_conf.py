9ce0b9190e6ae4604a88ba6b0d33519ec88d8b58
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2023-12-20 13:19:04 -0500

    added comment on options

--- docs/gallery_conf.py
+++ docs/gallery_conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
+import re
 
-from mkdocs_gallery.gen_gallery import DefaultResetArgv
 from mkdocs_gallery.sorting import FileNameSortKey
 
 min_reported_time = 0
@@ -9,12 +9,14 @@ if 'SOURCE_DATE_EPOCH' in os.environ:
     min_reported_time = sys.maxint if sys.version_info[0] == 2 else sys.maxsize
 
 # To be used as the "base" config,
-# this script is referenced in the mkdocs.yaml under `conf_script` option: docs/gallery_conf.py
+# mkdocs-gallery is a port of sphinx-gallery. For a detailed list
+# of configuration options see https://sphinx-gallery.github.io/stable/configuration.html
 conf = {
-    # reset args to default
-    'reset_argv': DefaultResetArgv(),
     # report runtime if larger than this value
     'min_reported_time': min_reported_time,
     # order your section in file name alphabetical order
-    'within_subsection_order': FileNameSortKey
+    'within_subsection_order': FileNameSortKey,
+    # run every script that matches pattern
+    # (here we match every file that ends in .py)
+    'filename_pattern': re.escape(os.sep) + r".+\.py$"
 }
