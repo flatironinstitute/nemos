d0f29048b10a27c6d084179bffc2153e8d9adac1
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-03-12 13:23:23 -0400

    linted

--- docs/neural_modeling/plot_03_grid_cells.py
+++ docs/neural_modeling/plot_03_grid_cells.py
@@ -17,19 +17,21 @@ from examples_utils import data
 
 # %%
 # ## DATA STREAMING
-# 
+#
 # Here we load the data from OSF. The data is a NWB file.
 #
 # <div class="notes">
 #   - Stream the data
 # </div>
 
-io = data.download_dandi_data("000582", "sub-11265/sub-11265_ses-07020602_behavior+ecephys.nwb",
-                                 )
+io = data.download_dandi_data(
+    "000582",
+    "sub-11265/sub-11265_ses-07020602_behavior+ecephys.nwb",
+)
 
 # %%
 # ## PYNAPPLE
-# 
+#
 #
 # <div class="notes">
 #   - Load the data with pynapple
@@ -41,7 +43,7 @@ dataset = nap.NWBFile(io.read())
 # Let's see what is in our data
 #
 # <div class="notes">
-#   - Print the data 
+#   - Print the data
 # </div>
 
 
@@ -50,7 +52,7 @@ print(dataset)
 
 # %%
 # In this case, the data were used in this [publication](https://www.science.org/doi/full/10.1126/science.1125572).
-# We thus expect to find neurons tuned to position and head-direction of the animal. 
+# We thus expect to find neurons tuned to position and head-direction of the animal.
 # Let's verify that with pynapple first.
 # Let's extract the spike times and the position of the animal.
 #
@@ -59,7 +61,7 @@ print(dataset)
 # </div>
 
 spikes = dataset["units"]  # Get spike timings
-position = dataset["SpatialSeriesLED1"] # Get the tracked orientation of the animal
+position = dataset["SpatialSeriesLED1"]  # Get the tracked orientation of the animal
 
 
 # %%
@@ -68,9 +70,9 @@ position = dataset["SpatialSeriesLED1"] # Get the tracked orientation of the ani
 # <div class="notes">
 #   - compute the head-direction of the animal from `SpatialSeriesLED1` and `SpatialSeriesLED1`
 # </div>
-diff = dataset['SpatialSeriesLED1'].values - dataset['SpatialSeriesLED2'].values
-head_dir = (np.arctan2(*diff.T) + (2*np.pi)) % (2*np.pi)
-head_dir = nap.Tsd(dataset['SpatialSeriesLED1'].index, head_dir).dropna()
+diff = dataset["SpatialSeriesLED1"].values - dataset["SpatialSeriesLED2"].values
+head_dir = (np.arctan2(*diff.T) + (2 * np.pi)) % (2 * np.pi)
+head_dir = nap.Tsd(dataset["SpatialSeriesLED1"].index, head_dir).dropna()
 
 
 # %%
@@ -81,17 +83,12 @@ head_dir = nap.Tsd(dataset['SpatialSeriesLED1'].index, head_dir).dropna()
 # </div>
 
 hd_tuning = nap.compute_1d_tuning_curves(
-    group=spikes, 
-    feature=head_dir,
-    nb_bins=61, 
-    minmax=(0, 2 * np.pi)
-    )
+    group=spikes, feature=head_dir, nb_bins=61, minmax=(0, 2 * np.pi)
+)
 
 pos_tuning, binsxy = nap.compute_2d_tuning_curves(
-    group=spikes, 
-    features=position, 
-    nb_bins=12)
-
+    group=spikes, features=position, nb_bins=12
+)
 
 
 # %%
@@ -99,20 +96,20 @@ pos_tuning, binsxy = nap.compute_2d_tuning_curves(
 # <div class="notes">
 #   - plot the tuning curves for each neurons
 # </div>
-fig = plt.figure(figsize = (12, 4))
+fig = plt.figure(figsize=(12, 4))
 gs = plt.GridSpec(2, len(spikes))
 for i in range(len(spikes)):
-    ax = plt.subplot(gs[0,i], projection='polar')
-    ax.plot(hd_tuning.loc[:,i])
-    
-    ax = plt.subplot(gs[1,i])
+    ax = plt.subplot(gs[0, i], projection="polar")
+    ax.plot(hd_tuning.loc[:, i])
+
+    ax = plt.subplot(gs[1, i])
     ax.imshow(gaussian_filter(pos_tuning[i], sigma=1))
 plt.tight_layout()
 
 
 # %%
 # ## NEMOS {.strip-code}
-# It's time to use nemos. 
+# It's time to use nemos.
 # Let's try to predict the spikes as a function of position and see if we can generate better tuning curves
 # First we start by binning the spike trains in 10 ms bins.
 #
@@ -120,7 +117,7 @@ plt.tight_layout()
 #   - bin spike trains in 10 ms bin size
 # </div>
 
-bin_size = 0.01 # second
+bin_size = 0.01  # second
 counts = spikes.count(bin_size, ep=position.time_support)
 
 # %%
@@ -141,11 +138,12 @@ position = position.interpolate(counts)
 #   - define a basis in 2D using nemos `RaisedCosineBasisLinear`
 # </div>
 
-basis_2d = nmo.basis.RaisedCosineBasisLinear(n_basis_funcs=10) * \
-            nmo.basis.RaisedCosineBasisLinear(n_basis_funcs=10)
+basis_2d = nmo.basis.RaisedCosineBasisLinear(
+    n_basis_funcs=10
+) * nmo.basis.RaisedCosineBasisLinear(n_basis_funcs=10)
 
 # %%
-# Let's see what a few basis look like 
+# Let's see what a few basis look like
 # Here we evaluate the basis on a 100x100 grid
 #
 # <div class="notes">
@@ -161,10 +159,10 @@ X, Y, Z = basis_2d.evaluate_on_grid(100, 100)
 #   - plot the evaluated basis
 # </div>
 
-fig, axs = plt.subplots(2,5, figsize=(10, 4))
+fig, axs = plt.subplots(2, 5, figsize=(10, 4))
 for k in range(2):
-  for h in range(5):
-    axs[k][h].contourf(X, Y, Z[:, :, 50+2*(k+h)], cmap='Blues')
+    for h in range(5):
+        axs[k][h].contourf(X, Y, Z[:, :, 50 + 2 * (k + h)], cmap="Blues")
 
 plt.tight_layout()
 
@@ -176,7 +174,9 @@ plt.tight_layout()
 #   - rescale the position between 0 and 1 to match the basis functions
 # </div>
 
-position = (position - np.min(position, 0)) / (np.max(position, 0) - np.min(position, 0))
+position = (position - np.min(position, 0)) / (
+    np.max(position, 0) - np.min(position, 0)
+)
 
 # %%
 # Now we can "evaluate" the basis for each position of the animal
@@ -185,35 +185,35 @@ position = (position - np.min(position, 0)) / (np.max(position, 0) - np.min(posi
 #   - evaluate the basis for each position of the animal
 # </div>
 
-position_basis = basis_2d.evaluate(position['x'], position['y'])
+position_basis = basis_2d.evaluate(position["x"], position["y"])
 
 # %%
 # Now try to make sense of what it is
 print(position_basis.shape)
 
 # %%
-# THe shape is (T, N_basis). It means for each time point, we evaluated the value of basis at the particular position 
+# THe shape is (T, N_basis). It means for each time point, we evaluated the value of basis at the particular position
 # Let's plot 5 time steps.
 
 
-fig = plt.figure(figsize = (12, 4))
+fig = plt.figure(figsize=(12, 4))
 gs = plt.GridSpec(2, 5)
 xt = np.arange(0, 1000, 200)
 cmap = plt.get_cmap("rainbow")
-colors = np.linspace(0,1, len(xt))
+colors = np.linspace(0, 1, len(xt))
 for cnt, i in enumerate(xt):
     ax = plt.subplot(gs[0, i // 200])
-    ax.imshow(position_basis[i].reshape(10, 10).T, origin='lower')
+    ax.imshow(position_basis[i].reshape(10, 10).T, origin="lower")
     for spine in ["top", "bottom", "left", "right"]:
         ax.spines[spine].set_color(cmap(colors[cnt]))
         ax.spines[spine].set_linewidth(3)
-    plt.title("T "+str(i))
+    plt.title("T " + str(i))
 
 ax = plt.subplot(gs[1, 2])
 
-ax.plot(position['x'][0:1000], position['y'][0:1000])
+ax.plot(position["x"][0:1000], position["y"][0:1000])
 for i in range(len(xt)):
-    ax.plot(position['x'][xt[i]], position['y'][xt[i]], 'o', color = cmap(colors[i]))
+    ax.plot(position["x"][xt[i]], position["y"][xt[i]], "o", color=cmap(colors[i]))
 
 plt.tight_layout()
 
@@ -228,8 +228,8 @@ plt.tight_layout()
 # </div>
 
 model = nmo.glm.GLM(
-        regularizer=nmo.regularizer.Ridge(regularizer_strength=1.0, solver_name="LBFGS")
-    )
+    regularizer=nmo.regularizer.Ridge(regularizer_strength=1.0, solver_name="LBFGS")
+)
 
 # %%
 # Let's fit the model
@@ -240,7 +240,7 @@ model = nmo.glm.GLM(
 
 neuron = 7
 
-model.fit(np.expand_dims(position_basis,1), counts[:, neuron:neuron+1])
+model.fit(np.expand_dims(position_basis, 1), counts[:, neuron : neuron + 1])
 
 
 # %%
@@ -250,16 +250,15 @@ model.fit(np.expand_dims(position_basis,1), counts[:, neuron:neuron+1])
 #   - predict the rate and compute a tuning curves using `compute_2d_tuning_curves_continuous` from pynapple
 # </div>
 
-rate_pos = model.predict(np.expand_dims(position_basis,1))
+rate_pos = model.predict(np.expand_dims(position_basis, 1))
 
 
 # %%
 # And compute a tuning curves again
 
 model_tuning, binsxy = nap.compute_2d_tuning_curves_continuous(
-    tsdframe=rate_pos * rate_pos.rate,
-    features=position,
-    nb_bins=12)
+    tsdframe=rate_pos * rate_pos.rate, features=position, nb_bins=12
+)
 
 
 # %%
@@ -275,12 +274,12 @@ smooth_model = gaussian_filter(model_tuning[0], sigma=1)
 vmin = min(smooth_pos_tuning.min(), smooth_model.min())
 vmax = max(smooth_pos_tuning.max(), smooth_model.max())
 
-fig = plt.figure(figsize = (12, 4))
+fig = plt.figure(figsize=(12, 4))
 gs = plt.GridSpec(1, 2)
 ax = plt.subplot(gs[0, 0])
-ax.imshow(gaussian_filter(pos_tuning[neuron], sigma=1), vmin=vmin,vmax=vmax)
+ax.imshow(gaussian_filter(pos_tuning[neuron], sigma=1), vmin=vmin, vmax=vmax)
 ax = plt.subplot(gs[0, 1])
-ax.imshow(gaussian_filter(model_tuning[0], sigma=1), vmin=vmin,vmax=vmax)
+ax.imshow(gaussian_filter(model_tuning[0], sigma=1), vmin=vmin, vmax=vmax)
 plt.tight_layout()
 
 
@@ -293,11 +292,12 @@ plt.tight_layout()
 #   - find the better `regularizer_strength` using `sklearn.model_selection.GriSearchCV`
 # </div>
 from sklearn.model_selection import GridSearchCV
+
 param_grid = dict(regularizer__regularizer_strength=[1e-6, 1e-3, 1])
 
 cls = GridSearchCV(model, param_grid=param_grid)
 
-cls.fit(np.expand_dims(position_basis, 1), counts[:, neuron: neuron+1])
+cls.fit(np.expand_dims(position_basis, 1), counts[:, neuron : neuron + 1])
 
 # %%
 # Let's get the best estimator and see what we get
@@ -317,9 +317,8 @@ best_model = cls.best_estimator_
 best_rate_pos = best_model.predict(np.expand_dims(position_basis, 1))
 
 best_model_tuning, binsxy = nap.compute_2d_tuning_curves_continuous(
-    tsdframe=best_rate_pos * best_rate_pos.rate,
-    features=position,
-    nb_bins=12)
+    tsdframe=best_rate_pos * best_rate_pos.rate, features=position, nb_bins=12
+)
 
 
 # %%
@@ -343,11 +342,3 @@ axs[1].imshow(smooth_model, vmin=vmin, vmax=vmax)
 axs[2].set_title(f"Ridge - strength: {best_model.regularizer.regularizer_strength}")
 axs[2].imshow(smooth_best_model, vmin=vmin, vmax=vmax)
 plt.tight_layout()
-
-
-
-
-
-
-
-
