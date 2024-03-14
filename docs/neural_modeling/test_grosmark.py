ef8875d2e3f7d679952a937862e7c06b98c6dfe0
Guillaume Viejo <guillaume.viejo@gmail.com>
2024-03-14 16:22:00 -0400

    updatge

--- docs/neural_modeling/test_grosmark.py
+++ docs/neural_modeling/test_grosmark.py
@@ -2,7 +2,7 @@
 # @Author: Guillaume Viejo
 # @Date:   2024-03-12 17:28:33
 # @Last Modified by:   Guillaume Viejo
-# @Last Modified time: 2024-03-14 12:55:17
+# @Last Modified time: 2024-03-14 16:19:42
 
 from matplotlib.pyplot import *
 import numpy as np
@@ -11,6 +11,7 @@ import pynapple as nap
 import os
 import nemos as nmo
 
+from sklearn.model_selection import GridSearchCV
 
 # ################
 # units = loadmat("/mnt/home/gviejo/Downloads/Achilles_10252013_spikes_cellinfo.mat")
@@ -30,9 +31,9 @@ import nemos as nmo
 # theta = nap.Tsd(t=theta_info['theta']['time'], d=theta_info['theta']['phase'])
 # ################
 
-spikes = nap.load_file(os.path.expanduser("/Users/ebalzani/Downloads/Achilles_10252013/Achilles_10252013_spikes.npz"))
-position = nap.load_file(os.path.expanduser("/Users/ebalzani/Downloads/Achilles_10252013/Achilles_10252013_position.npz"))
-theta = nap.load_file(os.path.expanduser("/Users/ebalzani/Downloads/Achilles_10252013/Achilles_10252013_theta.npz"))
+spikes = nap.load_file(os.path.expanduser("~/Dropbox/Achilles_10252013/Achilles_10252013_spikes.npz"))
+position = nap.load_file(os.path.expanduser("~/Dropbox/Achilles_10252013/Achilles_10252013_position.npz"))
+theta = nap.load_file(os.path.expanduser("~/Dropbox/Achilles_10252013/Achilles_10252013_theta.npz"))
 
 
 # only the pyr
@@ -81,15 +82,34 @@ X = basis.evaluate(position, phase, speed)
 
 X = X[:,None,:]
 
-neuron = 10
+neuron = 77
 
-glm = nmo.glm.GLM(regularizer=nmo.regularizer.UnRegularized())
+glm = nmo.glm.GLM(regularizer=nmo.regularizer.Lasso(regularizer_strength=1e-5, solver_kwargs=dict(tol=10**-12)))
+#glm = nmo.glm.GLM(regularizer=nmo.regularizer.UnRegularized("LBFGS", solver_kwargs=dict(tol=10**-12)))
 glm.fit(X, count[:,[neuron]])
 
+# reg = {"regularizer__regularizer_strength":np.logspace(-5, -1, 5)}
 
-w_pos = glm.coef_[0, 0:10]
-w_phase = glm.coef_[0, 10:22]
-w_speed = glm.coef_[0, -15:]
+# cls = GridSearchCV(glm, reg)
+# cls.fit(X, count[:, [neuron]])
+
+# glm = cls.best_estimator_
+
+
+w_speed = glm.coef_[0,-15:]
+
+w_pos_ph = glm.coef_[0,0:120]
+
+XX, YY, Z = (position_basis*phase_basis).evaluate_on_grid(50, 50)
+out = np.einsum('ijk,k->ij', Z, glm.coef_[0,0:120])
+
+
+tmp = nap.TsdFrame(t=position.t, d=np.vstack((position.d, phase.d)).T, time_support = position.time_support)
+tc_pos_ph, xybins = nap.compute_2d_tuning_curves(spikes, tmp, 20)
+
+tmp = nap.TsdFrame(t=position.t, d=np.vstack((position.d, speed.d)).T, time_support = position.time_support)
+tc_pos_sp, xybins = nap.compute_2d_tuning_curves(spikes, tmp, 20)
+
 
 
 samples, eval_basis = speed_basis.evaluate_on_grid(100)
@@ -113,15 +133,27 @@ tc_pf_glm = nap.compute_1d_tuning_curves_continuous(pr, position, 50, position.t
 
 figure()
 subplot(221)
-plot(np.dot(eval_basis, w_speed))
+plot(np.dot(eval_basis, w_speed)+glm.intercept_)
 subplot(222)
 plot(tc_sp.values[:,neuron])
 plot(tc_sp_glm.values[:,0])
 subplot(224)
 plot(tc_pf.values[:,neuron])
 plot(tc_pf_glm.values[:,0])
+# show()
+
+
+figure()
+subplot(131)
+imshow(out, origin='lower', aspect='auto')
+subplot(132)
+imshow(tc_pos_ph[list(tc_pos_ph.keys())[neuron]], origin='lower', aspect='auto')
+
 show()
 
+
+import sys
+sys.exit()
 
 figure()
 
@@ -133,12 +165,16 @@ figure()
 for i in range(10*11):
 	subplot(10, 11, i+1)
 	fill_between(tc_pf.index.values, np.zeros(len(tc_pf)), tc_pf.values[:,i])
-
+	title(i)
+	xticks([])
+	yticks([])
 figure()
 for i in range(10*11):
 	subplot(10, 11, i+1)
 	fill_between(tc_sp.index.values, np.zeros(len(tc_sp)), tc_sp.values[:,i])
 	title(i)
+	xticks([])
+	yticks([])
 
 show()
 
