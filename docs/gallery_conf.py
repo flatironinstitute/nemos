616af074145adb49fce0dd93726cd006163e8695
Bence Bagi <bagi.bence94@gmail.com>
2024-06-11 18:02:38 -0400

    Add documentation page for pipelining and CV

--- docs/gallery_conf.py
+++ docs/gallery_conf.py
@@ -5,7 +5,7 @@ import sys
 from mkdocs_gallery.sorting import FileNameSortKey
 
 min_reported_time = 0
-if 'SOURCE_DATE_EPOCH' in os.environ:
+if "SOURCE_DATE_EPOCH" in os.environ:
     min_reported_time = sys.maxint if sys.version_info[0] == 2 else sys.maxsize
 
 # To be used as the "base" config,
@@ -13,11 +13,11 @@ if 'SOURCE_DATE_EPOCH' in os.environ:
 # of configuration options see https://sphinx-gallery.github.io/stable/configuration.html
 conf = {
     # report runtime if larger than this value
-    'min_reported_time': min_reported_time,
+    "min_reported_time": min_reported_time,
     # order your section in file name alphabetical order
-    'within_subsection_order': FileNameSortKey,
+    "within_subsection_order": FileNameSortKey,
     # run every script that matches pattern
     # (here we match every file that ends in .py)
-    'filename_pattern': re.escape(os.sep) + r"plot_.+\.py$",
-    'ignore_pattern': r"_plot_.+\.py$"
+    "filename_pattern": re.escape(os.sep) + r"plot_.+\.py$",
+    "ignore_pattern": r"(_plot_.+\.py$|_helpers\.py$)",
 }
