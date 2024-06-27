6bcdb3fe8256c19708fd11344903c3cf9f11bdda
Bence Bagi <bagi.bence94@gmail.com>
2024-06-27 16:40:56 -0400

    Switch inheritance between SVRG and ProxSVRG

--- src/nemos/solvers.py
+++ src/nemos/solvers.py
@@ -7,6 +7,7 @@ from jax import grad, jit, lax, random
 from jax._src.typing import ArrayLike
 from jaxopt import OptStep
 from jaxopt._src import loop
+from jaxopt.prox import prox_none
 from jaxopt.tree_util import (
     tree_add,
     tree_add_scalar_mul,
@@ -30,10 +31,11 @@ class SVRGState(NamedTuple):
     df_xs: Optional[tuple] = None
 
 
-class SVRG:
+class ProxSVRG:
     def __init__(
         self,
         fun: Callable,
+        prox: Callable,
         maxiter: int = 1000,
         key: Optional[KeyArrayLike] = None,
         # N: Optional[int] = None,
@@ -48,12 +50,14 @@ class SVRG:
         self.tol = tol
         self.loss_gradient = jit(grad(self.fun, argnums=(0,)))
 
+        self.proximal_operator = prox
+
     def init_state(self, init_params, *args, **kwargs):
-        if len(args) == 0:
+        if len(args) < 2:
             N = None
             df_xs = None
         else:
-            X, y = args
+            prox_lambda, X, y = args
 
             assert isinstance(X, ArrayLike)
             assert isinstance(y, ArrayLike)
@@ -72,116 +76,10 @@ class SVRG:
             df_xs=df_xs,
         )
         return state
-
-    def update(self, xk, state, *args, **kwargs):
-        # possibly batch data
-        x, y = args
-
-        # if the state hasn't been initialized with the full gradient,
-        # the best we can do is initialize df_xs with the gradient of the current mini-batch
-        # not the full gradient, but less noisy than any xk
-        if state.xs is None or state.df_xs is None:
-            state = state._replace(
-                xs=xk,
-                df_xs=self.loss_gradient(xk, x, y)[0],
-            )
-
-        xs, df_xs = state.xs, state.df_xs
-
-        # don't carry x and y, they should be cached on the first call
-        def loop_body(i, xk):
-            # no random sampling, just iterate through the data points
-            dfik_xk = self.loss_gradient(xk, x[i], y[i])[0]
-            dfik_xs = self.loss_gradient(xs, x[i], y[i])[0]
-            gk = jax.tree_util.tree_map(
-                lambda a, b, c: a - b + c, dfik_xk, dfik_xs, df_xs
-            )
-            xk = tree_add_scalar_mul(xk, -state.stepsize, gk)
-            return xk
-
-        xk = lax.fori_loop(0, x.shape[0], loop_body, xk)
-
-        # xs is updated outside for this implementation
-        # because we only want to update it after a whole sweep
-        # through the data, not after every mini-batch
-        next_state = state._replace(
-            iter_num=state.iter_num + 1,
-        )
-
-        return OptStep(params=xk, state=next_state)
-
-    # def update(self, xk, state, *args, **kwargs):
-    #    return self._sgd_update(xk, state, *args, **kwargs)
-
-    def run(self, init_params, *args, **kwargs):
-        # this method assumes that args hold the full data
-        def body_fun(step):
-            xs_prev, state = step
-            # evaluate and store the full gradient with the params from the last inner loop
-            state = state._replace(
-                df_xs=self.loss_gradient(xs_prev, *args)[0],
-            )
-
-            # update xs with the final xk after running through the whole data
-            xs, state = self.update(xs_prev, state, *args, **kwargs)
-
-            state = state._replace(
-                xs=xs,
-                error=self._error(xs, xs_prev, state.stepsize),
-            )
-
-            return OptStep(params=xs, state=state)
-
-        def cond_fun(step):
-            _, state = step
-            return (state.iter_num <= self.maxiter) & (state.error >= self.tol)
-
-        init_state = self.init_state(init_params, *args)
-
-        final_xs, final_state = loop.while_loop(
-            cond_fun=cond_fun,
-            body_fun=body_fun,
-            init_val=OptStep(params=init_params, state=init_state),
-            maxiter=self.maxiter,
-            jit=True,
-        )
-        return OptStep(params=final_xs, state=final_state)
-
-    # @staticmethod
-    # def _error(x, x_prev, stepsize):
-    #    diff_norm = tree_l2_norm(tree_sub(x, x_prev))
-    #    return diff_norm / stepsize
-
-    @staticmethod
-    def _error(x, x_prev, stepsize):
-        return tree_l2_norm(tree_sub(x, x_prev)) / tree_l2_norm(x_prev)
-
-
-class ProxSVRG(SVRG):
-    def __init__(
-        self,
-        fun: Callable,
-        prox: Callable,
-        maxiter: int = 1000,
-        key: Optional[KeyArrayLike] = None,
-        # N: Optional[int] = None,
-        stepsize: float = 1e-3,
-        tol: float = 1e-5,
-    ):
-        super().__init__(
-            fun,
-            maxiter,
-            key,
-            # N,
-            stepsize,
-            tol,
-        )
-        self.proximal_operator = prox
-
-    def init_state(self, init_params, *args, **kwargs):
         prox_lambda, X, y = args
         return super().init_state(init_params, X, y, **kwargs)
 
+    @partial(jit, static_argnums=(0,))
     def update(self, xk, state, *args, **kwargs):
         """
         Performs the inner loop of Prox-SVRG
@@ -213,8 +111,8 @@ class ProxSVRG(SVRG):
             )
 
             xk = tree_add_scalar_mul(xk, -state.stepsize, gk)
-            xk = self.proximal_operator(xk, state.stepsize * prox_lambda)
-            # xk = self.proximal_operator(xk, prox_lambda, scaling=state.stepsize)
+            # xk = self.proximal_operator(xk, state.stepsize * prox_lambda)
+            xk = self.proximal_operator(xk, prox_lambda, scaling=state.stepsize)
 
             x_sum = tree_add(x_sum, xk)
 
@@ -240,6 +138,7 @@ class ProxSVRG(SVRG):
         )
         return OptStep(params=xs, state=next_state)
 
+    @partial(jit, static_argnums=(0,))
     def run(self, init_params, *args, **kwargs):
         prox_lambda, X, y = args
 
@@ -265,7 +164,7 @@ class ProxSVRG(SVRG):
             _, state = step
             return (state.iter_num <= self.maxiter) & (state.error >= self.tol)
 
-        init_state = self.init_state(init_params, *args)
+        init_state = self.init_state(init_params, prox_lambda, X, y)
 
         final_xs, final_state = loop.while_loop(
             cond_fun=cond_fun,
@@ -275,3 +174,50 @@ class ProxSVRG(SVRG):
             jit=True,
         )
         return OptStep(params=final_xs, state=final_state)
+
+    # @staticmethod
+    # def _error(x, x_prev, stepsize):
+    #    diff_norm = tree_l2_norm(tree_sub(x, x_prev))
+    #    return diff_norm / stepsize
+
+    @staticmethod
+    def _error(x, x_prev, stepsize):
+        return tree_l2_norm(tree_sub(x, x_prev)) / tree_l2_norm(x_prev)
+
+
+class SVRG(ProxSVRG):
+    def __init__(
+        self,
+        fun: Callable,
+        maxiter: int = 1000,
+        key: Optional[KeyArrayLike] = None,
+        # N: Optional[int] = None,
+        stepsize: float = 1e-3,
+        tol: float = 1e-5,
+    ):
+        super().__init__(
+            fun,
+            prox_none,
+            maxiter,
+            key,
+            # N,
+            stepsize,
+            tol,
+        )
+
+    def init_state(self, init_params, *args, **kwargs):
+        # substitute None for prox_lambda
+        if len(args) == 2:
+            args = (None, *args)
+        return super().init_state(init_params, *args, **kwargs)
+
+    @partial(jit, static_argnums=(0,))
+    def update(self, xk, state, *args, **kwargs):
+        if len(args) == 2:
+            args = (None, *args)
+        return super().update(xk, state, *args, **kwargs)
+
+    @partial(jit, static_argnums=(0,))
+    def run(self, init_params, *args, **kwargs):
+        args = (None, *args)
+        return super().run(init_params, *args, **kwargs)
