fbf44e4c5d71fd3ddf843c0fa83db979162a6590
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-08-27 19:33:36 +0200

    added test for last 3 tags

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,16 +1,21 @@
-import os.path
+import pathlib
 
 import pytest
+
 import nemos as nmo
 from nemos.fetch.fetch_data import REGISTRY_UTILS
 
 
 def test_registry():
-    """Check that the current tag is in registry"""
+    """Check that the current tag is in the registry."""
     assert nmo.__version__ in REGISTRY_UTILS
 
 
-# use the pytest fixture tmpdir which is a test specific temporary directory
-def test_download_utils(tmpdir):
-    fhname = nmo.fetch.fetch_utils(tmpdir)
-    assert fhname.exists()
+@pytest.mark.parametrize("version", list(REGISTRY_UTILS.keys())[-3:])
+def test_download_utils(version, tmpdir):
+    """Test that utils can be downloaded.
+
+    This use the pytest fixture tmpdir which is a test specific temporary directory.
+    """
+    fhname = nmo.fetch.fetch_utils(tmpdir, version=version)
+    assert pathlib.Path(fhname).exists()
