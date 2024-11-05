2f5481d8610adabb0c024882375c2da90c2dcded
Edoardo Balzani <edoardo.balzani87@gmail.com>
2024-11-05 09:49:58 -0500

    Update docs/tutorials/plot_02_head_direction.py
    Co-authored-by: William F. Broderick <billbrod@gmail.com>

--- docs/tutorials/plot_02_head_direction.py
+++ docs/tutorials/plot_02_head_direction.py
@@ -583,7 +583,8 @@ tuning = nap.compute_1d_tuning_curves_continuous(predicted_firing_rate,
 weights = basis.split_by_feature(model.coef_, axis=0)
 
 # the output is a dictionary containing an array of shape (n_neurons, n_basis_funcs, n_neurons)
-print(f"{weights.keys()}: {weights['convolved counts'].shape}")
+for k, v in weights.items():
+    print(f"{k}: {v.shape}")
 
 # get the array
 weights = weights["convolved counts"]
