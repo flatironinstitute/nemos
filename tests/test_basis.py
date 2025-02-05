7d9e5e4a90f72cf21975a6b2d37e6af757ec928d
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-02-05 16:27:38 -0500

    added test on dynamic label update

--- tests/test_basis.py
+++ tests/test_basis.py
@@ -5257,6 +5257,91 @@ def test_label_uniqueness_enforcing(bas1, bas2, bas3, basis_class_specific_param
                     comb.set_params(**{f'{comb.basis1.label}__label': lab2})
 
 
+@pytest.mark.parametrize("bas", list_all_basis_classes())
+def test_dynamic_disambiguate_label(bas, basis_class_specific_params):
+    if bas in (AdditiveBasis, MultiplicativeBasis, basis.TransformerBasis):
+        return
+
+    combine_basis = CombinedBasis()
+    bas_instance = combine_basis.instantiate_basis(
+        5, bas, basis_class_specific_params, window_size=10,
+    )
+    assert bas_instance.label == bas_instance.__class__.__name__
+    add_12 = bas_instance + bas_instance
+    mul_12 = bas_instance * bas_instance
+    assert add_12.basis1.label == bas_instance.__class__.__name__
+    assert add_12.basis2.label == (bas_instance.__class__.__name__ + "_1")
+    assert mul_12.basis1.label == bas_instance.__class__.__name__
+    assert mul_12.basis2.label == (bas_instance.__class__.__name__ + "_1")
+
+    # check labels
+    add_123 = add_12 + bas_instance
+    mul_123 = mul_12 * bas_instance
+    mix_123 = add_12 * bas_instance
+    assert add_123.basis1.basis1.label == bas_instance.__class__.__name__
+    assert add_123.basis1.basis2.label == (bas_instance.__class__.__name__ + "_1")
+    assert add_123.basis2.label == (bas_instance.__class__.__name__ + "_2")
+
+    assert mul_123.basis1.basis1.label == bas_instance.__class__.__name__
+    assert mul_123.basis1.basis2.label == (bas_instance.__class__.__name__ + "_1")
+    assert mul_123.basis2.label == (bas_instance.__class__.__name__ + "_2")
+
+    assert mix_123.basis1.basis1.label == bas_instance.__class__.__name__
+    assert mix_123.basis1.basis2.label == (bas_instance.__class__.__name__ + "_1")
+    assert mix_123.basis2.label == (bas_instance.__class__.__name__ + "_2")
+
+    assert add_123.basis1.label == "(" + bas_instance.__class__.__name__ + " + " +  bas_instance.__class__.__name__ + "_1)"
+    assert mul_123.basis1.label == "(" + bas_instance.__class__.__name__ + " * " +  bas_instance.__class__.__name__ + "_1)"
+    assert mix_123.basis1.label == "(" + bas_instance.__class__.__name__ + " + " +  bas_instance.__class__.__name__ + "_1)"
+
+    # change label leaves
+    add_123.basis1.basis1.label = "x"
+    mul_123.basis1.basis1.label = "x"
+    mix_123.basis1.basis1.label = "x"
+    assert add_123.basis1.basis1.label == "x"
+    assert add_123.basis1.basis2.label == bas_instance.__class__.__name__
+    assert add_123.basis2.label == (bas_instance.__class__.__name__ + "_1")
+
+    assert mul_123.basis1.basis1.label == "x"
+    assert mul_123.basis1.basis2.label == bas_instance.__class__.__name__
+    assert mul_123.basis2.label == (bas_instance.__class__.__name__ + "_1")
+
+    assert mix_123.basis1.basis1.label == "x"
+    assert mix_123.basis1.basis2.label == bas_instance.__class__.__name__
+    assert mix_123.basis2.label == (bas_instance.__class__.__name__ + "_1")
+
+    assert add_123.basis1.label == "(" + "x" + " + " + bas_instance.__class__.__name__ + ")"
+    assert mul_123.basis1.label == "(" + "x" + " * " + bas_instance.__class__.__name__ + ")"
+    assert mix_123.basis1.label == "(" + "x" + " + " + bas_instance.__class__.__name__ + ")"
+
+    assert add_123.label == f"((x + {bas_instance.__class__.__name__}) + {bas_instance.__class__.__name__}_1)"
+    assert mul_123.label == f"((x * {bas_instance.__class__.__name__}) * {bas_instance.__class__.__name__}_1)"
+    assert mix_123.label == f"((x + {bas_instance.__class__.__name__}) * {bas_instance.__class__.__name__}_1)"
+
+    # change composite label
+    add_123.basis1.label = "y"
+    mul_123.basis1.label = "y"
+    mix_123.basis1.label = "y"
+
+    assert add_123.basis1.label == "y"
+    assert mul_123.basis1.label == "y"
+    assert mix_123.basis1.label == "y"
+
+    assert add_123.basis1.basis1.label == "x"
+    assert mul_123.basis1.basis1.label == "x"
+    assert mix_123.basis1.basis1.label == "x"
+
+    assert add_123.basis1.basis2.label == bas_instance.__class__.__name__
+    assert mul_123.basis1.basis2.label == bas_instance.__class__.__name__
+    assert mix_123.basis1.basis2.label == bas_instance.__class__.__name__
+
+    assert add_123.label == f"(y + {bas_instance.__class__.__name__}_1)"
+    assert mul_123.label == f"(y * {bas_instance.__class__.__name__}_1)"
+    assert mix_123.label == f"(y * {bas_instance.__class__.__name__}_1)"
+
+    assert add_123.basis2.label == f"{bas_instance.__class__.__name__}_1"
+    assert mul_123.basis2.label == f"{bas_instance.__class__.__name__}_1"
+    assert mix_123.basis2.label == f"{bas_instance.__class__.__name__}_1"
 
 
 
