feb4434d99f883a7514cc1aac50fd33de740b61d
William F. Broderick <billbrod@gmail.com>
2025-02-13 12:45:08 -0500

    remove unnecessary improt

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,6 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import nemos
 import sys, os
 from pathlib import Path
 
