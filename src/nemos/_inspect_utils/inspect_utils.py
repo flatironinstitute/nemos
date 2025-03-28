656ec5318f7d571b9b36c64189ba38ec5fd6a22f
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-03-28 09:30:47 -0400

    removed unused funcs in inspect

--- src/nemos/_inspect_utils/inspect_utils.py
+++ src/nemos/_inspect_utils/inspect_utils.py
@@ -1,6 +1,6 @@
 import abc
 import inspect
-from typing import Callable, List, Tuple
+from typing import List, Tuple
 
 
 def reimplements_method(
@@ -245,39 +245,3 @@ def trim_kwargs(cls: type, kwargs: dict, class_specific_params: dict):
         for key, value in kwargs.items()
         if key in class_specific_params[cls.__name__]
     }
-
-
-def count_params_by_kind(func: Callable, kind: set[inspect.Parameter.kind]):
-    """Count how many parameters of the callable are of the desired kind.
-
-    In a callable definition, the parameter kind is one of the following:
-
-    - POSITIONAL_ONLY: A parameter that can only be specified positionally
-      (i.e., it cannot be passed as a keyword argument).
-
-    - POSITIONAL_OR_KEYWORD: A parameter that can be passed either positionally or as a keyword argument.
-
-    - KEYWORD_ONLY: A parameter that must be passed as a keyword argument
-      (appears after `*args` in function signatures).
-
-    - VAR_POSITIONAL: A variable-length positional argument (`*args`),
-      which collects extra positional arguments.
-
-    - VAR_KEYWORD: A variable-length keyword argument (`**kwargs`),
-      which collects extra keyword arguments.
-    """
-    sig = inspect.signature(func)
-    params = sig.parameters.values()
-    return sum(1 for p in params if p.kind in kind)
-
-
-def count_positional_and_var_args(func: Callable):
-    """Count the positional arguments of a callable."""
-    num_positional_args = count_params_by_kind(
-        func,
-        {inspect.Parameter.POSITIONAL_ONLY, inspect.Parameter.POSITIONAL_OR_KEYWORD},
-    )
-    num_var_args = count_params_by_kind(
-        func, {inspect.Parameter.VAR_KEYWORD, inspect.Parameter.VAR_POSITIONAL}
-    )
-    return num_positional_args, num_var_args
