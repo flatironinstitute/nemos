4f2de19aecfa6996fbd0696827769cc090fd318b
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-04-03 16:02:11 -0400

    fix test and push

--- tests/test_basis.py
+++ tests/test_basis.py
@@ -6064,7 +6064,7 @@ def test_all_public_importable_bases_equal():
         if issubclass(obj, nemos.basis._basis.Basis)
     }
 
-    if public_bases != imported_bases:
+    if public_bases.difference(imported_bases) != {"CustomBasis"}:
         raise ValueError(
             "nemos/basis/__init__.py imported basis objects does not match"
             " nemos/basis/_composition_utils.py's __PUBLIC_BASES__ list:\n"
