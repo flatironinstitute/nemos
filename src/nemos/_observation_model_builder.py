ac653031267239347d3b63c35148f5acca65fa52
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-04-11 09:39:01 -0700

    linted

--- src/nemos/_observation_model_builder.py
+++ src/nemos/_observation_model_builder.py
@@ -1,6 +1,10 @@
-from .observation_models import GammaObservations, PoissonObservations
+from .observation_models import (
+    BernoulliObservations,
+    GammaObservations,
+    PoissonObservations,
+)
 
-AVAILABLE_OBSERVATION_MODELS = ["Poisson", "Gamma"]
+AVAILABLE_OBSERVATION_MODELS = ["Bernoulli", "Gamma", "Poisson"]
 
 
 def instantiate_observation_model(observation_model: str):
@@ -25,6 +29,8 @@ def instantiate_observation_model(observation_model: str):
         return PoissonObservations()
     elif observation_model == "Gamma":
         return GammaObservations()
+    elif observation_model == "Bernoulli":
+        return BernoulliObservations()
     else:
         raise ValueError(
             f"Unknown observation model: {observation_model}. "
