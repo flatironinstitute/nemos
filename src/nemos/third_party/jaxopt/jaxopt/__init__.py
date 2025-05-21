13f3e9eb2cf7a0c6701147267128e07d987dd676
Bence Bagi <bagi.bence94@gmail.com>
2025-05-21 11:06:16 +0200

    Put back jaxopt deprecation warning

--- src/nemos/third_party/jaxopt/jaxopt/__init__.py
+++ src/nemos/third_party/jaxopt/jaxopt/__init__.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import warnings
 
 from nemos.third_party.jaxopt.jaxopt import implicit_diff
 from nemos.third_party.jaxopt.jaxopt import isotonic
@@ -23,7 +24,9 @@ from nemos.third_party.jaxopt.jaxopt._src.anderson import AndersonAcceleration
 from nemos.third_party.jaxopt.jaxopt._src.anderson_wrapper import AndersonWrapper
 from nemos.third_party.jaxopt.jaxopt._src.armijo_sgd import ArmijoSGD
 from nemos.third_party.jaxopt.jaxopt._src.base import OptStep
-from nemos.third_party.jaxopt.jaxopt._src.backtracking_linesearch import BacktrackingLineSearch
+from nemos.third_party.jaxopt.jaxopt._src.backtracking_linesearch import (
+    BacktrackingLineSearch,
+)
 from nemos.third_party.jaxopt.jaxopt._src.bfgs import BFGS
 from nemos.third_party.jaxopt.jaxopt._src.bisection import Bisection
 from nemos.third_party.jaxopt.jaxopt._src.block_cd import BlockCoordinateDescent
@@ -31,11 +34,17 @@ from nemos.third_party.jaxopt.jaxopt._src.broyden import Broyden
 from nemos.third_party.jaxopt.jaxopt._src.cd_qp import BoxCDQP
 from nemos.third_party.jaxopt.jaxopt._src.cvxpy_wrapper import CvxpyQP
 from nemos.third_party.jaxopt.jaxopt._src.eq_qp import EqualityConstrainedQP
-from nemos.third_party.jaxopt.jaxopt._src.fixed_point_iteration import FixedPointIteration
+from nemos.third_party.jaxopt.jaxopt._src.fixed_point_iteration import (
+    FixedPointIteration,
+)
 from nemos.third_party.jaxopt.jaxopt._src.gauss_newton import GaussNewton
 from nemos.third_party.jaxopt.jaxopt._src.gradient_descent import GradientDescent
-from nemos.third_party.jaxopt.jaxopt._src.hager_zhang_linesearch import HagerZhangLineSearch
-from nemos.third_party.jaxopt.jaxopt._src.iterative_refinement import IterativeRefinement
+from nemos.third_party.jaxopt.jaxopt._src.hager_zhang_linesearch import (
+    HagerZhangLineSearch,
+)
+from nemos.third_party.jaxopt.jaxopt._src.iterative_refinement import (
+    IterativeRefinement,
+)
 from nemos.third_party.jaxopt.jaxopt._src.lbfgs import LBFGS
 from nemos.third_party.jaxopt.jaxopt._src.lbfgsb import LBFGSB
 from nemos.third_party.jaxopt.jaxopt._src.levenberg_marquardt import LevenbergMarquardt
@@ -53,3 +62,9 @@ from nemos.third_party.jaxopt.jaxopt._src.scipy_wrappers import ScipyLeastSquare
 from nemos.third_party.jaxopt.jaxopt._src.scipy_wrappers import ScipyMinimize
 from nemos.third_party.jaxopt.jaxopt._src.scipy_wrappers import ScipyRootFinding
 from nemos.third_party.jaxopt.jaxopt._src.zoom_linesearch import ZoomLineSearch
+
+warnings.warn(
+    "JAXopt is no longer maintained. See https://docs.jax.dev/en/latest/ for"
+    " alternatives.",
+    DeprecationWarning,
+)
