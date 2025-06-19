f85addab9a44347ca54528a652db289bcdd26d4e
Bence Bagi <bagi.bence94@gmail.com>
2025-06-19 17:10:05 +0200

    Export the wrappers

--- src/nemos/solvers/__init__.py
+++ src/nemos/solvers/__init__.py
@@ -1,7 +1,14 @@
 """Custom solvers module."""
 
-from ._svrg import SVRG, ProxSVRG
+from ._svrg import SVRG, ProxSVRG, WrappedSVRG, WrappedProxSVRG
 from ._svrg_defaults import (
     glm_softplus_poisson_l_max_and_l,
     svrg_optimal_batch_and_stepsize,
+)
+
+from .jaxopt_solvers import (
+    JaxoptGradientDescent,
+    JaxoptProximalGradient,
+    JaxoptBFGS,
+    JaxoptLBFGS,
 )
