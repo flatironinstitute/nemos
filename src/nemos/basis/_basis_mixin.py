fix masking

src/nemos/basis/_basis_mixin.py
@@ -230,6 +230,17 @@ class BasisMixin:
         else:
             slice_dict = self._get_feature_slicing(double_complex=False)[0]
         # expand complex output to real and imag, drop imag component if real
+        column_idx_info = {
+            key: (sl.stop - sl.start, self[key].n_basis_funcs)
+            for key, sl in slice_dict.items()
+        }
+
+        # define a mask
+        def mask_array(length, step):
+            mask = np.ones(length, dtype=bool)
+            mask[::step] = False
+            return mask
+
         X = np.concatenate(
             [
                 (
@@ -239,7 +250,9 @@ class BasisMixin:
                         [
                             np.real(X[..., sl]),
                             (
-                                np.imag(X[..., sl])[..., 1:]
+                                np.imag(X[..., sl])[
+                                    ..., mask_array(*column_idx_info[key])
+                                ]
                                 if self[key]._include_constant
                                 else np.imag(X[..., sl])
                             ),

src/nemos/basis/_composition_utils.py
@@ -640,7 +640,9 @@ def _get_n_output_features(
                 return None
             n_output_features += (
                 scale * n_features_1 * n_features_2
-                - b._include_constant * double_complex
+                - b._include_constant
+                * double_complex
+                * int(np.prod(b._input_shape_product))
             )
         else:
             n_features = _atomic_basis_get_n_basis_output_features(b)