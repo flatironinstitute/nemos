a8491b5dec610f7643f6764ec159c95e1b98b53e
Bence Bagi <bagi.bence94@gmail.com>
2025-06-26 16:04:19 +0200

    Update solver registry

--- src/nemos/solvers/_solver_registry.py
+++ src/nemos/solvers/_solver_registry.py
@@ -1,17 +1,34 @@
-from . import (
+from .jaxopt_solvers import (
     JaxoptGradientDescent,
     JaxoptProximalGradient,
     JaxoptBFGS,
     JaxoptLBFGS,
+)
+
+from ._svrg import (
     WrappedProxSVRG,
     WrappedSVRG,
 )
 
+from .optimistix_solvers import (
+    OptimistixBFGS,
+    # OptimistixLBFGS,
+)
+
+from ._prox_grad import (
+    OptaxOptimistixProximalGradient,
+)
+
 solver_registry = {
-    "GradientDescent": JaxoptGradientDescent,  # jaxopt.GradientDescent,
-    "ProximalGradient": JaxoptProximalGradient,
+    "GradientDescent": JaxoptGradientDescent,
+    # "ProximalGradient": JaxoptProximalGradient,
+    "ProximalGradient": OptaxOptimistixProximalGradient,
+    #
     "LBFGS": JaxoptLBFGS,
-    "BFGS": JaxoptBFGS,
+    # "LBFGS": OptimistixLBFGS,
+    #
+    "BFGS": OptimistixBFGS,
+    #
     "SVRG": WrappedSVRG,
     "ProxSVRG": WrappedProxSVRG,
 }
