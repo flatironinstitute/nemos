4f3c0bd2c35ada5f14804ef62c70d1141f8e1388
Bence Bagi <bagi.bence94@gmail.com>
2025-06-26 16:02:01 +0200

    Add generic types to AbstractSolver

--- src/nemos/solvers/abstract_solver.py
+++ src/nemos/solvers/abstract_solver.py
@@ -1,15 +1,19 @@
 import abc
+from typing import Generic, TypeVar
+
+SolverState = TypeVar("SolverState")
+StepResult = TypeVar("StepResult")
 
 
-class AbstractSolver(abc.ABC):
+class AbstractSolver(abc.ABC, Generic[SolverState, StepResult]):
     @abc.abstractmethod
-    def init_state(self, init_params, *args):
+    def init_state(self, init_params, *args) -> SolverState:
         pass
 
     @abc.abstractmethod
-    def update(self, params, state, *args):
+    def update(self, params, state, *args) -> StepResult:
         pass
 
     @abc.abstractmethod
-    def run(self, init_params, *args):
+    def run(self, init_params, *args) -> StepResult:
         pass
