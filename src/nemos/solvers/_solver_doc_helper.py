90a1401a5ad4bfb3854df355b1dacc4b19a0873e
Bence Bagi <bagi.bence94@gmail.com>
2025-08-08 12:36:37 +0200

    Improve get_solver_documentation and add it to the API reference

--- src/nemos/solvers/_solver_doc_helper.py
+++ src/nemos/solvers/_solver_doc_helper.py
@@ -1,23 +1,39 @@
 from typing import Type
 import inspect
 import re
+from pydoc import render_doc
 
 from ._solver_registry import solver_registry
 
 
 # TODO: Add a short how-to guide about this.
-# TODO: Add this to the API reference
-def get_solver_documentation(solver: str | Type) -> str:
+def get_solver_documentation(solver: str | Type, show_help: bool = False) -> str:
     """
-    Get the documentation of a specified solver, including the docstring of its `__init__`.
+    Get the documentation of a specified solver, including accepted arguments and the docstring of its `__init__`.
 
-    `solver` can be a string or a type (e.g. `nemos.solvers.JaxoptGradientDescent`).
-    If `solver` is a string, the corresponding solver will be read from the solver registry.
+    Parameters
+    ----------
+    solver:
+        `solver` can be a string or a type (e.g. `nemos.solvers.JaxoptGradientDescent`).
+        If `solver` is a string, the corresponding solver will be read from the solver registry.
+    show_help:
+        Instead of the docstring, show the full output that be produced by `help(solver)` where `solver` is a type.
     """
     if isinstance(solver, str):
         solver = solver_registry[solver]
 
-    doc = inspect.getdoc(solver) or f"No documentation found for {solver}."
+    if show_help:
+        return render_doc(solver, title="Help on %s")
+
+    solver_class_path = f"{solver.__module__}.{solver.__name__}"
+    intro = inspect.cleandoc(
+        f"""
+        Showing docstring of {solver_class_path}.
+        For potentially more info, use `show_help=True`.
+        """
+    )
+    solver_doc = inspect.getdoc(solver) or f"No documentation found for {solver}."
+    doc = intro + "\n\n" + solver_doc
 
     # if it doesn't already have it, then expand the docs
     # with the __init__'s docstring'
