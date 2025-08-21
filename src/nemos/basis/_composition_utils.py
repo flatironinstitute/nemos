1c489a6bc038ec7c13d0bffb190b3a72f112268f
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-08-21 23:25:57 +0200

    fix list len

--- src/nemos/basis/_composition_utils.py
+++ src/nemos/basis/_composition_utils.py
@@ -463,7 +463,7 @@ def get_input_shape(bas: "BasisMixin") -> List[Tuple | None]:
         ishape = getattr(bas, "_input_shape_", [None])
         return ishape if ishape is not None else [None]
     elif not hasattr(bas, "basis1") and hasattr(bas, "_input_shape_"):
-        return [None] if bas._input_shape_ is None else bas._input_shape_
+        return [None] * input_dim if bas._input_shape_ is None else bas._input_shape_
 
     basis1 = getattr(bas, "_basis1", None)
     basis2 = getattr(bas, "_basis2", None)
