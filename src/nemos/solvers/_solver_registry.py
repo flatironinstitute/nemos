8c12b99d666b57b46f215082e795fe43cd15a1c1
Bence Bagi <bagi.bence94@gmail.com>
2025-10-09 17:04:12 +0200

    Extend list_available_solvers docstring

--- src/nemos/solvers/_solver_registry.py
+++ src/nemos/solvers/_solver_registry.py
@@ -31,10 +31,20 @@ def list_available_solvers():
     """
     List the available solvers that can be used for fitting models.
 
+    To access an extended documentation about a specific solver,
+    see `get_solver_documentation`.
+
     Example
     -------
     >>> import nemos as nmo
     >>> nmo.solvers.list_available_solvers()
     ['GradientDescent', 'ProximalGradient', 'LBFGS', 'BFGS', 'SVRG', 'ProxSVRG', 'NonlinearCG']
+    >>> print(nmo.solvers.get_solver_documentation("SVRG"))
+    Showing docstring of nemos.solvers._svrg.WrappedSVRG.
+    For potentially more info, use `show_help=True`.
+    <BLANKLINE>
+    Adapter for NeMoS's implementation of SVRG following the AbstractSolver interface.
+    <BLANKLINE>
+    ...
     """
     return list(solver_registry.keys())
