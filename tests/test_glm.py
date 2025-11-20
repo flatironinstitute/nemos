9905185914db2caf5eb1d4a20293afcd83c4fe24
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-11-20 14:21:21 -0500

    remove all refs to old func naming

--- tests/test_glm.py
+++ tests/test_glm.py
@@ -3081,7 +3081,7 @@ class TestGLMObservationModel:
         glm.instantiate_solver(glm.compute_loss)
 
         # NOTE these two are not the same because for example Ridge augments the loss
-        # loss_grad = jax.jit(jax.grad(glm._predict_and_compute_loss))
+        # loss_grad = jax.jit(jax.grad(glm.compute_loss))
         loss_grad = jax.jit(jax.grad(glm._solver_loss_fun))
 
         # copied from GLM.fit
