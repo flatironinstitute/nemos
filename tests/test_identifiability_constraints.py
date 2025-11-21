defa6c4ba8f75e88597d7d4d5318873e21dc4440
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-11-20 19:26:12 -0500

    add more samples for float 32 precison to work

--- tests/test_identifiability_constraints.py
+++ tests/test_identifiability_constraints.py
@@ -92,7 +92,7 @@ def test_apply_identifiability_constraints_add_constant(add_intercept, expected_
 @pytest.mark.parametrize(
     "basis, input_shape, output_shape, expected_columns",
     [
-        (RaisedCosineLinearEval(10, width=4), (50,), (50, 10), jnp.arange(10)),
+        (RaisedCosineLinearEval(10, width=4), (100,), (100, 10), jnp.arange(10)),
         (
             BSplineEval(5) + BSplineEval(6),
             (20,),
