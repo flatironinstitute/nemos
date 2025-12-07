5b48716fea517a2e710a2ecf0b6ac4f667e2b260
Bence Bagi <bagi.bence94@gmail.com>
2025-12-07 12:44:03 +0100

    Define _inexact_asarray using jnp.asarray
    Stop importing private optx._misc and use short redefinition.
    
    JAX issue 15676 seems to be fixed, so no need to use
    optimistix._misc.asarray and the helper functions it relies on.

--- src/nemos/solvers/_aux_helpers.py
+++ src/nemos/solvers/_aux_helpers.py
@@ -2,15 +2,35 @@ from typing import Any, Callable
 
 import equinox as eqx
 import jax
-from optimistix._misc import asarray, inexact_asarray
+import jax.numpy as jnp
 
 from ..typing import Params, Pytree
+
+
+def _inexact_asarray(x):
+    """
+    Cast x as inexact array.
+
+    Adaptation of optimistix._misc.inexact_asarray using
+    jnp.asarray instead of optimistix._misc.asarray.
+    """
+    dtype = jnp.result_type(x)
+    if not jnp.issubdtype(jnp.result_type(x), jnp.inexact):
+        if jax.config.jax_enable_x64:  # pyright: ignore
+            dtype = jnp.float64
+        else:
+            dtype = jnp.float32
+
+    # using jnp.asarray instead of optimistix._misc.asarray
+    # as the relevant JAX issue seems to be fixed
+    # https://github.com/jax-ml/jax/issues/15676
+    return jnp.asarray(x, dtype)
 
 
 def _as_inexact_array(params: Pytree):
     """Cast PyTree as an inexact jnp.array."""
     # adapted from optimistix._misc
-    return jax.tree_util.tree_map(inexact_asarray, params)
+    return jax.tree_util.tree_map(_inexact_asarray, params)
 
 
 def _wrap_aux(fn: Callable) -> Callable:
@@ -45,8 +65,8 @@ def _out_asarray(fn: Callable):
 
     def _fn_with_array_outputs(*args, **kwargs):
         out, aux = fn(*args, **kwargs)
-        out = jax.tree_util.tree_map(inexact_asarray, out)
-        aux = jax.tree_util.tree_map(asarray, aux)
+        out = jax.tree_util.tree_map(_inexact_asarray, out)
+        aux = jax.tree_util.tree_map(jnp.asarray, aux)
         return out, aux
 
     return _fn_with_array_outputs
