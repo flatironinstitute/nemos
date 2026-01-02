b7f688200bd0cc87bf82eb9f87da9c5386352af3
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2026-01-02 16:45:52 -0500

    remove todo

--- tests/test_base_regressor_subclasses.py
+++ tests/test_base_regressor_subclasses.py
@@ -844,9 +844,7 @@ class TestObservationModel:
     "instantiate_base_regressor_subclass",
     [
         {"model": m, "obs_model": "Poisson", "simulate": True}
-        # TODO: REMOVE WHEN FIT IS IMPLEMENTED
         for m in MODEL_REGISTRY.keys()
-        if m != "GLMHMM"
     ],
     indirect=True,
 )
