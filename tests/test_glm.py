9bcc1066ef69801dbf35b410b60cbd16238a009d
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2026-01-15 21:25:10 -0500

    re add test to param

--- tests/test_glm.py
+++ tests/test_glm.py
@@ -1457,11 +1457,11 @@ class TestGLM:
     "model_instantiation",
     [
         "categoricalGLM_model_instantiation",
-        # "gaussianGLM_model_instantiation",
-        # "poissonGLM_model_instantiation",
-        # "gammaGLM_model_instantiation",
-        # "bernoulliGLM_model_instantiation",
-        # "negativeBinomialGLM_model_instantiation",
+        "gaussianGLM_model_instantiation",
+        "poissonGLM_model_instantiation",
+        "gammaGLM_model_instantiation",
+        "bernoulliGLM_model_instantiation",
+        "negativeBinomialGLM_model_instantiation",
     ],
 )
 class TestGLMObservationModel:
