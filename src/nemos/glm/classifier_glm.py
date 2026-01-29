a9dcf668f920493f224e91d7887ad48543c5467f
wulfdewolf <wolf2wulf@gmail.com>
2026-01-29 12:55:31 +0000

    missing import

--- src/nemos/glm/classifier_glm.py
+++ src/nemos/glm/classifier_glm.py
@@ -4,7 +4,7 @@
 from __future__ import annotations
 
 from numbers import Number
-from typing import Callable, Literal, Optional, Tuple, Union
+from typing import Callable, Literal, Optional, Tuple, Union, Any
 
 import jax
 import jax.numpy as jnp
