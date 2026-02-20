217fb84ad05e78b804bf330ade3dfccdabe5732a
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2026-02-20 11:14:37 -0500

    remove lazy validation import

--- src/nemos/__init__.py
+++ src/nemos/__init__.py
@@ -6,29 +6,7 @@ from importlib.metadata import version as _get_version
 import lazy_loader as _lazy
 
 # All submodules are lazy-loaded for faster import times
-__getattr__, __dir__, __all__ = _lazy.attach(
-    __name__,
-    submodules=[
-        "basis",
-        "convolve",
-        "exceptions",
-        "fetch",
-        "glm",
-        "identifiability_constraints",
-        "observation_models",
-        "pytrees",
-        "regularizer",
-        "simulation",
-        "solvers",
-        "styles",
-        "tree_utils",
-        "type_casting",
-        "utils",
-    ],
-    submod_attrs={
-        "io.io": ["inspect_npz", "load_model"],
-    },
-)
+__getattr__, __dir__, __all__ = _lazy.attach_stub(__name__, __file__)
 
 
 try:
---
+++ src/nemos/__init__.pyi
@@ -0,0 +1,40 @@
+"""Stubs."""
+
+from . import (
+    basis,
+    convolve,
+    exceptions,
+    fetch,
+    glm,
+    identifiability_constraints,
+    observation_models,
+    pytrees,
+    regularizer,
+    simulation,
+    solvers,
+    styles,
+    tree_utils,
+    type_casting,
+    utils,
+)
+from .io.io import inspect_npz, load_model
+
+__all__ = [
+    "basis",
+    "convolve",
+    "exceptions",
+    "fetch",
+    "glm",
+    "identifiability_constraints",
+    "observation_models",
+    "pytrees",
+    "regularizer",
+    "simulation",
+    "solvers",
+    "styles",
+    "tree_utils",
+    "type_casting",
+    "utils",
+    "inspect_npz",
+    "load_model",
+]
