2915b9c4a3a37680b72763174f74a5980dbd9e87
Arina Medvedeva <akuchuruk@gmail.com>
2026-03-27 11:39:20 -0400

    added core log likelohood computation

---
+++ src/nemos/pp_glm/basis.py
@@ -0,0 +1,37 @@
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+
+jax.config.update("jax_enable_x64", True)
+
+
+@partial(jax.jit, static_argnums=(1, 2, 3, 4))
+def raised_cosine_log_eval(x, n_basis_funcs, ws, width=2.0, time_scaling=50.0):
+    """jax only raised cosine log."""
+    last_peak = 1 - width / (n_basis_funcs + width - 1)
+    peaks = jnp.linspace(0, last_peak, n_basis_funcs)
+    delta = peaks[1] - peaks[0]
+
+    x = x / ws
+
+    # this makes sure that the out of range are set to zero
+    x = jnp.where(jnp.abs(x) > 1, 1, x)
+
+    x = jnp.log(time_scaling * x + 1) / jnp.log(time_scaling + 1)
+
+    basis_funcs = 0.5 * (
+        jnp.cos(
+            jnp.clip(
+                jnp.pi * (x[:, None] - peaks[None]) / (delta * width),
+                -jnp.pi,
+                jnp.pi,
+            )
+        )
+        + 1
+    )
+    return basis_funcs
+
+def RaisedCosineLogEval(n_basis_funcs, ws, width=2.0, time_scaling=50.0):
+    eval_func = lambda dts: raised_cosine_log_eval(dts, n_basis_funcs, ws, width, time_scaling)
+    return eval_func
