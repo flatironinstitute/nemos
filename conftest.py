2463b52f051c37e3c1b3f54194b3f5a2a2c8f72f
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2026-06-04 09:39:24 -0400

    add close figs in tests

--- conftest.py
+++ conftest.py
@@ -1,5 +1,9 @@
 """Register a custom option in root."""
 
+import matplotlib.pyplot as plt
+import pytest
+
+
 def pytest_addoption(parser):
     parser.addoption(
         "--timeit",
@@ -7,3 +11,16 @@ def pytest_addoption(parser):
         default=False,
         help="Show aggregated parametrized test durations",
     )
+
+
+# following https://github.com/scverse/scanpy/issues/1662
+@pytest.fixture(autouse=True)
+def close_figures_on_teardown():
+    """
+    Close figures when exiting from doctests.
+
+    Note that this won't close between tests in a given docstring, but it will close
+    between docstrings.
+    """  # numpydoc ignore=YD01
+    yield
+    plt.close("all")
