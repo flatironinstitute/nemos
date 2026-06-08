a0db2b42a3ddee9b9346d13a04de0f456de8caa6
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2026-06-08 09:29:44 -0400

    fix repr tests

--- tests/test_glm.py
+++ tests/test_glm.py
@@ -1792,8 +1792,9 @@ class TestGLMObservationModel:
         """
         Fixture for test_repr_out
         """
+        # GLMs override the regularizer default with a model-level default (Newton)
         default_solver_name = nmo.solvers.get_solver(
-            nmo.regularizer.UnRegularized().default_solver
+            nmo.glm.GLM._default_solver
         ).algo_name
         lbfgs_solver_name = nmo.solvers.get_solver("LBFGS").algo_name
         if "poisson" in model_instantiation:
